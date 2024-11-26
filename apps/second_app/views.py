8cf12ddac0cbd41e72902a672b7e07216410beed
Hemlata <hemlata.raghuvanshi@tricera.tech>
2024-11-27 00:21:15 +0530

    Add templates for second_app or completed template task

--- apps/second_app/views.py
+++ apps/second_app/views.py
@@ -5,10 +5,30 @@ from django.http import HttpResponse
 # Create your views here.
 
 def demo1(request, id):
-    return HttpResponse(f"<h1 style='color:red'>I am on demo1 page{id}</h1><hr><p>This is the paragraph of our page</p><style></style>")
+    context = {
+    'cars': [
+      {
+        'brand': 'Ford',
+        'model': 'Mustang',
+        'year': '1964',
+      },
+      {
+        'brand': 'Ford',
+        'model': 'Bronco',
+        'year': '1970',
+      },
+      {
+        'brand': 'Volvo',
+        'model': 'P1800',
+        'year': '1964',
+      }]
+    }
+    return render(request, 'demo1.html', context)
+    # return HttpResponse(f"<h1 style='color:red'>I am on demo1 page{id}</h1><hr><p>This is the paragraph of our page</p><style></style>")
 
 def demo2(request):
-    return HttpResponse("<h1>I am on demo2 page</h1>")
+    context = {'name': 'Vishakha'}
+    return render(request, 'demo2.html', context)
 def demo3(request):
     return HttpResponse("<h1>I am on demo3 page</h1>")
 
