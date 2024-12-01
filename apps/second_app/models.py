8f9f021ef753c8548104f7f5d1b27ba03d6bce44
Hemlata <hemlata.raghuvanshi@tricera.tech>
2024-12-01 22:57:22 +0530

    Add initial model in second app

--- apps/second_app/models.py
+++ apps/second_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Transactions(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.IntegerField()
+    description = models.TextField(null=True, blank=True)
+    transaction_date = models.DateField()
+
+
+class Students(models.Model):
+    stu_id = models.IntegerField()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(Students,on_delete=models.CASCADE)
+    bio = models.TextField(null=True, blank=True)
