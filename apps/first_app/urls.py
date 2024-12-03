5b562255ec94858ac75100fbfb72e88ea2df3d87
Hemlata <hemlata.raghuvanshi@tricera.tech>
2024-12-03 19:51:52 +0530

    Add Task-105(Create Class based View)

--- apps/first_app/urls.py
+++ apps/first_app/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import hello_world, about, contact
+from .views import hello_world, about, contact, MyView
 
 urlpatterns = [
+
     path('home/<int:id>', hello_world ),
     path('about', about),
     path('contact', contact),
+    path('cl', MyView.as_view())
 ]
